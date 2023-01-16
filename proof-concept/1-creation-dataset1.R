69aae1968f383aa0ca7fb2613ea68638dd7f4bf8
VictoriaDiCesare <79263847+VictoriaDiCesare@users.noreply.github.com>
2023-01-16 11:25:59 +0100

    Export data and plots (commented) to Google Drive

--- proof-concept/1-creation-dataset1.R
+++ proof-concept/1-creation-dataset1.R
@@ -101,8 +101,6 @@ for(i in researchers$researcher_id) {
   # Add to framework
   df1[nrow(df1) + 1,] <- v
 }
-# Export
-write.csv(df1, file = "G:/Mi unidad/1. Work sync/Projects/2 In progress/2021_Cassidy/proof_of_concept/df1.txt")
 
 
 ###
@@ -195,7 +193,8 @@ ggplot(df1_final_grouped_country, aes(x = country_code_origin, y = researchers_c
   ggtitle("Number of researchers by country of origin and mobility/affiliation") +
   xlab("Country of origin") +
   ylab("Number of researchers")
-ggsave("0A.png")
+#ggsave("0A.png")
+#drive_update(file = as_id("1Dvg5vAf3GI1J_9pRbQOk9fx-r98KfGAw"), media = "0A.png")
 
 # 0B) grouped barplot: RESEARCHERS by COUNTRY and MOB/AFFIL (all but non-mobile)
 ggplot(df1_final_grouped_country_nonmob_filter, aes(x = country_code_origin, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -204,7 +203,8 @@ ggplot(df1_final_grouped_country_nonmob_filter, aes(x = country_code_origin, y =
   ggtitle("Number of researchers by country of origin and mobility/affiliation (all but non-mobile)") +
   xlab("Country of origin") +
   ylab("Number of researchers")
-ggsave("0B.png")
+#ggsave("0B.png")
+#drive_update(file = as_id("1Slcq8vEm5bXpbEdzWNF3ylBqY2y1P4Di"), media = "0B.png")
 
 # 1A) boxplot: MOB/AFFIL by TOTAL_P (<= 100)
 ggplot(df1_final_p100_filter, aes(x = total_p, y = mob_affil, fill = mob_affil, color = mob_affil)) +
@@ -213,7 +213,8 @@ ggplot(df1_final_p100_filter, aes(x = total_p, y = mob_affil, fill = mob_affil, 
   ggtitle("Mobility/affiliation of researchers by number of publications (<=100)") +
   xlab("Number of publications") +
   ylab("Mobility/Affiliation")
-ggsave("1A.png")
+#ggsave("1A.png")
+#drive_update(file = as_id("10-NnuhBgLTi_5yh25NiCBW_JZYCzJNOj"), media = "1A.png")
 
 # 1B) boxplot: MOB/AFFIL by TOTAL_TOPICS (<= 30)
 ggplot(df1_final_t30_filter, aes(x = total_topics, y = mob_affil, fill = mob_affil, color = mob_affil)) +
@@ -222,7 +223,8 @@ ggplot(df1_final_t30_filter, aes(x = total_topics, y = mob_affil, fill = mob_aff
   ggtitle("Mobility/affiliation of researchers by number of topics (<= 30)") +
   xlab("Number of topics") +
   ylab("Mobility/Affiliation")
-ggsave("1B.png")
+#ggsave("1B.png")
+#drive_update(file = as_id("1WnNQQx3zIqIUEz_v-CAE-MPrxz-U4TAS"), media = "1B.png")
 
 # 2.2A) boxplot: TOTAL_P (<=150) by ACTIVE_YEARS and MOB/AFFIL
 ggplot(df1_final_p150_filter, aes(x = active_years, y = total_p, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -231,7 +233,8 @@ ggplot(df1_final_p150_filter, aes(x = active_years, y = total_p, fill = factor(m
   ggtitle("Number of publications (<=150) by researchers' active years and mobility/affiliation") +
   xlab("Active years") +
   ylab("Number of publications")
-ggsave("22A.png")
+#ggsave("22A.png")
+#drive_update(file = as_id("1mWBHZW4XeCvFpSOM4yfp1XibMNaKMdrn"), media = "22A.png")
 
 # 2.2B) boxplot: TOTAL_TOPICS (<= 40) by ACTIVE_YEARS and MOB/AFFIL
 ggplot(df1_final_t40_filter, aes(x = active_years, y = total_topics, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -240,7 +243,8 @@ ggplot(df1_final_t40_filter, aes(x = active_years, y = total_topics, fill = fact
   ggtitle("Number of topics (1:40) by researchers' active years and mobility/affiliation") +
   xlab("Active years") +
   ylab("Number of topics")
-ggsave("22B.png")
+#ggsave("22B.png")
+#drive_update(file = as_id("1pqfTG7j5hcU_n6uPJ4tMHitENb25FbCy"), media = "22B.png")
 
 # 2.2C) boxplot: CL_P by ACTIVE_YEARS and MOB/AFFIL
 ggplot(df1_final, aes(x = active_years, y = cl_p, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -249,7 +253,8 @@ ggplot(df1_final, aes(x = active_years, y = cl_p, fill = factor(mob_affil), colo
   ggtitle("Topics' sizes by researchers' active years and mobility/affiliation") +
   xlab("Active years") +
   ylab("Topic size (median pubs)")
-ggsave("22C.png")
+#ggsave("22C.png")
+#drive_update(file = as_id("1XxGNlpP471Sbemq-iRBwJ1BMyEA_0DxY"), media = "22C.png")
 
 # 2.2D) boxplot: CL_INT by ACTIVE_YEARS and MOB/AFFIL
 ggplot(df1_final, aes(x = active_years, y = proportion_cl_int, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -258,7 +263,8 @@ ggplot(df1_final, aes(x = active_years, y = proportion_cl_int, fill = factor(mob
   ggtitle("Proportion of international topics by researchers' active years and mobility/affiliation") +
   xlab("Active years") +
   ylab("Proportion of international topics")
-ggsave("22D.png")
+#ggsave("22D.png")
+#drive_update(file = as_id("1AeOjJQch2L9nlhYWT7t4pTP4GBSWGo7j"), media = "22D.png")
 
 # 5A) boxplots: TOTAL_P (<=150) by ACTIVE_YEARS, COUNTRY and MOB/AFFIL
 ggplot(df1_final_p150_filter, aes(x = active_years, y = total_p, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -268,7 +274,8 @@ ggplot(df1_final_p150_filter, aes(x = active_years, y = total_p, fill = factor(m
   ggtitle("Number of publications (<=150) by researchers' active years, country and mobility/affiliation") +
   xlab("Active years") +
   ylab("Number of publications")
-ggsave("5A.png")
+#ggsave("5A.png")
+#drive_update(file = as_id("1hosOMEARJO_U5YhS8Zyg21flaJCuFnzd"), media = "5A.png")
 
 # 5B) boxplots: TOTAL_TOPICS (<= 40) by ACTIVE_YEARS, COUNTRY and MOB/AFFIL
 ggplot(df1_final_t40_filter, aes(x = active_years, y = total_topics, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -278,7 +285,8 @@ ggplot(df1_final_t40_filter, aes(x = active_years, y = total_topics, fill = fact
   ggtitle("Number of topics (1:40) by researchers' active years, country and mobility/affiliation") +
   xlab("Active years") +
   ylab("Number of topics")
-ggsave("5B.png")
+#ggsave("5B.png")
+#drive_update(file = as_id("1T4S1gChrspYLRdpUF_YqL_KbIRkOFy13"), media = "5B.png")
 
 # 5C) boxplots: CL_P by ACTIVE_YEARS, COUNTRY and MOB/AFFIL
 ggplot(df1_final, aes(x = active_years, y = cl_p, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -288,7 +296,8 @@ ggplot(df1_final, aes(x = active_years, y = cl_p, fill = factor(mob_affil), colo
   ggtitle("Topics' sizes by researchers' active years, country and mobility/affiliation") +
   xlab("Active years") +
   ylab("Topic size (median pubs)")
-ggsave("5C.png")
+#ggsave("5C.png")
+#drive_update(file = as_id("16mOMoc0YWUK_zLg4B93x5PY2Zn9hE5Ae"), media = "5C.png")
 
 # 5D) boxplots: CL_INT BY ACTIVE_YEARS, COUNTRY and MOB/AFFIL
 ggplot(df1_final, aes(x = active_years, y = proportion_cl_int, fill = factor(mob_affil), color = factor(mob_affil))) +
@@ -298,7 +307,8 @@ ggplot(df1_final, aes(x = active_years, y = proportion_cl_int, fill = factor(mob
   ggtitle("Proportion of international topics' by researchers' active years, country and mobility/affiliation") +
   xlab("Active years") +
   ylab("Proportion of international topics")
-ggsave("5D.png")
+#ggsave("5D.png")
+#drive_update(file = as_id("1izt0UsLAs0tx6TsSVK2OsVJrM5uHENaB"), media = "5D.png")
 
 # 6A) grouped barplot: RESEARCHERS by ACTIVE_YEARS and MOB/AFFIL
 ggplot(df1_final_grouped_years, aes(x = active_years, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -307,7 +317,8 @@ ggplot(df1_final_grouped_years, aes(x = active_years, y = researchers_count, fil
   ggtitle("Number of researchers by active years and mobility/affiliation") +
   xlab("Active years") +
   ylab("Number of researchers")
-ggsave("6A.png")
+#ggsave("6A.png")
+#drive_update(file = as_id("1ybda7E3heFnp716Qg1dbZvKB4vBYjzaH"), media = "6A.png")
 
 # 6B) grouped barplot: RESEARCHERS by ACTIVE_YEARS and MOB/AFFIL (all but non-mobile)
 ggplot(df1_final_grouped_years_nonmob_filter, aes(x = active_years, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -316,7 +327,8 @@ ggplot(df1_final_grouped_years_nonmob_filter, aes(x = active_years, y = research
   ggtitle("Number of researchers by active years and mobility/affiliation (all but non-mobile)") +
   xlab("Active years") +
   ylab("Number of researchers")
-ggsave("6B.png")
+#ggsave("6B.png")
+#drive_update(file = as_id("1QnBHwu-o01UXJS1VrMEMpGdf2UiTZ-Ux"), media = "6B.png")
 
 # 6C) grouped barplot: RESEARCHERS by ACTIVE_YEARS, COUNTRY and MOB/AFFIL
 ggplot(df1_final_grouped_years_country, aes(x = active_years, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -326,7 +338,8 @@ ggplot(df1_final_grouped_years_country, aes(x = active_years, y = researchers_co
   ggtitle("Number of researchers by active years, country and mobility/affiliation") +
   xlab("Active years") +
   ylab("Number of researchers")
-ggsave("6C.png")
+#ggsave("6C.png")
+#drive_update(file = as_id("1H586JgVQbYZDpazA6PxMDHfkPyKf2qtJ"), media = "6C.png")
 
 # 6D) grouped barplot: RESEARCHERS by ACTIVE_YEARS, COUNTRY and MOB/AFFIL (all but non-mobile)
 ggplot(df1_final_grouped_years_country_nonmob_filter, aes(x = active_years, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -336,7 +349,8 @@ ggplot(df1_final_grouped_years_country_nonmob_filter, aes(x = active_years, y = 
   ggtitle("Number of researchers by active years, country and mobility/affiliation (all but non-mobile)") +
   xlab("Active years") +
   ylab("Number of researchers")
-ggsave("6D.png")
+#ggsave("6D.png")
+#drive_update(file = as_id("1cV6exhxQkjuKwzRjnZ13QWmR_ZYV8akM"), media = "6D.png")
 
 # 7A) grouped barplot: RESEARCHERS by TOTAL_P (<=50) and MOB/AFFIL
 ggplot(df1_final_grouped_p50_filter, aes(x = total_p, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -345,7 +359,8 @@ ggplot(df1_final_grouped_p50_filter, aes(x = total_p, y = researchers_count, fil
   ggtitle("Number of researchers by number of publications (<=50) and mobility/affiliation") +
   xlab("Number of publications") +
   ylab("Number of researchers")
-ggsave("7A.png")
+#ggsave("7A.png")
+#drive_update(file = as_id("1Po0fJuv4VmaNvMoNR0-5T5X66jc0f9pK"), media = "7A.png")
 
 # 7B) grouped barplot: RESEARCHERS by TOTAL_P (<=50) and MOB/AFFIL (all but non-mobile)
 ggplot(df1_final_grouped_p50_nonmob_filter, aes(x = total_p, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -354,7 +369,8 @@ ggplot(df1_final_grouped_p50_nonmob_filter, aes(x = total_p, y = researchers_cou
   ggtitle("Number of researchers by number of publications (<=50) and mobility/affiliation (all but non-mobile)") +
   xlab("Number of publications") +
   ylab("Number of researchers")
-ggsave("7B.png")
+#ggsave("7B.png")
+#drive_update(file = as_id("1YeV8yHKclm91DAcxuzMNPOJIj_W2dbSG"), media = "7B.png")
 
 # 7C) grouped barplot: RESEARCHERS by TOTAL_P (<=50), COUNTRY and MOB/AFFIL
 ggplot(df1_final_grouped_p50_country_filter, aes(x = total_p, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -364,7 +380,8 @@ ggplot(df1_final_grouped_p50_country_filter, aes(x = total_p, y = researchers_co
   ggtitle("Number of researchers by number of publications (<=50), country and mobility/affiliation") +
   xlab("Number of publications") +
   ylab("Number of researchers")
-ggsave("7C.png")
+#ggsave("7C.png")
+#drive_update(file = as_id("1MgU3WiJjsvqisDOLGtJHEdTpNr6imqTB"), media = "7C.png")
 
 # 7D) grouped barplot: RESEARCHERS by TOTAL_P (<=50), COUNTRY and MOB/AFFIL (all but non-mobile)
 ggplot(df1_final_grouped_p50_country_nonmob_filter, aes(x = total_p, y = researchers_count, fill = mob_affil, color = mob_affil)) +
@@ -374,7 +391,8 @@ ggplot(df1_final_grouped_p50_country_nonmob_filter, aes(x = total_p, y = researc
   ggtitle("Number of researchers by number of publications (<=50), country and mobility/affiliation (all but non-mobile)") +
   xlab("Number of publications") +
   ylab("Number of researchers")
-ggsave("7D.png")
+#ggsave("7D.png")
+#drive_update(file = as_id("1HEPHg4gIpLvzclrKsr5nF22-kvGUS1Bq"), media = "7D.png")
 
 # 8A) joyplot: ACTIVE_YEARS by TOTAL_TOPICS (<=40)
 ggplot(df1_final_t40_filter, aes(x = total_topics, y = active_years, fill = active_years, color = active_years)) +
@@ -384,7 +402,8 @@ ggplot(df1_final_t40_filter, aes(x = total_topics, y = active_years, fill = acti
   ggtitle("Researchers' active years by number of topics (<=40)") +
   xlab("Number of topics") +
   ylab("Active years")
-ggsave("8A.png")
+#ggsave("8A.png")
+#drive_update(file = as_id("1-xdO8b7pNvoGIzyr4NPnya0tp0H2LHP0"), media = "8A.png")
 
 # 8B) joyplot: ACTIVE_YEARS by TOTAL_TOPICS (<=40) and COUNTRY
 ggplot(df1_final_t40_filter, aes(x = total_topics, y = active_years, fill = active_years, color = active_years)) +
@@ -395,4 +414,10 @@ ggplot(df1_final_t40_filter, aes(x = total_topics, y = active_years, fill = acti
   ggtitle("Researchers' active years by number of topics (<=40) and country") +
   xlab("Number of topics") +
   ylab("Active years")
-ggsave("8B.png")
+#ggsave("8B.png")
+#drive_update(file = as_id("1LthxsYPF3_SpDGXcfQzLu49x30ArqS7q"), media = "8B.png")
+
+
+# Export data to Google Drive
+write.csv(df1, file = "df1.txt")
+drive_update(file = as_id("1w94k-mZ7R66KNRMQvQcrS7TimmI-aDHI"), media = "df1.txt")
