d6d47cc9370f146d3de0980e0bda0a943a236d69
elrobin <elrobinster@gmail.com>
2023-02-01 09:22:51 +0100

    Changed parameter to update file in GDrive

--- proof-concept/2-creation-dataset2.R
+++ proof-concept/2-creation-dataset2.R
@@ -237,6 +237,5 @@ df2.str[,-1] <- mutate_all(df2.str[,-1], function(x) as.numeric(x))
 df2 <- df2.str
 # Export data to Google Drive
 write.csv(df2, file = "C:/Users/elrobinster/Downloads/df2.txt") # Add local path
-drive_upload(media = "C:/Users/elrobinster/Downloads/df2.txt", 
-             path = as_id("1xIBEu0WX-Xq4aqbkM4C46RRDQoPO981r"),
-             name = "df2.txt")
+drive_update(media = "C:/Users/elrobinster/Downloads/df2.txt", 
+             path = as_id("1xIBEu0WX-Xq4aqbkM4C46RRDQoPO981r"))
